02c560e1da3f88d7dd30f9cd6bc05bef34ece61a
pranab-ext-1997 <pranab_bhunia@yahoo.com>
2025-03-23 22:55:31 +0000

    data_type_note

---
+++ dataTypes_summery.js
@@ -0,0 +1,32 @@
+// premetiveDatatype
+
+// 7 types:String,Number,Boolean,null,undefined,Symbol,BignInt
+
+// const score= 100
+// const scoreValue= 100.3
+// const isLoggedIn = false
+// const outSideTemp = null
+// let userEmail;
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
+// console.log(id===anotherId);
+// const bigNumber = 787878787878787878787878n
+// console.log(bigNumber);
+
+
+
+//RefrerenceType(NonPremetive)
+
+// Array,Object,Functions
+
+const heros= ["saktiman","naagraj","doga"];
+
+let myObj = {
+    name:"pranab",
+    age:28
+}
+
+const myFunction =function (){
+    console.log("Hi, Pranab");
+    
+}
