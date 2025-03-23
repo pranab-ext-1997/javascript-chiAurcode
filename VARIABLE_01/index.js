5b95e7378665ebac8a791fa8c0c05e515712518f
pranab-ext-1997 <pranab_bhunia@yahoo.com>
2025-03-23 21:36:37 +0000

    datatypes

---
+++ VARIABLE_01/index.js
@@ -0,0 +1,17 @@
+console.log("pranab")
+
+// const accountId= 435535
+
+let accEmail="pbhunia05@gmail.com"
+
+var accountPassoword="2134"
+
+accountCity="Kolkata"
+let accountState;
+
+accountId=2
+console.table([accountId,accEmail,accountPassoword,accountCity,accountState])
+
+
+/*Prefer not to use var 
+because of issue in block and functional scope*/
---
+++ datatypes/index.js
@@ -0,0 +1,16 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3)//we are using node js,not browser
+
+// number
+// BigInt
+// String
+// Boolean
+// null
+// undefined
+// symbol=> to define quique
+
+
+
+// Object
+console.table([typeof "pranab",typeof null,typeof undefined]);
